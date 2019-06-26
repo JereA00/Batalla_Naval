5d7847a974123eb95cb42cbbf8dce6f7c3f179a5
Jeremias <jeremias_davidenco@hotmail.com>
2019-06-26 01:16:41 -0300

    123

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+import Modelo.Celda;
+import Modelo.Excepciones.InvalidDisparoException;
+import Modelo.Excepciones.InvalidPosicionBarco;
+import Modelo.FactoryBarcos.BarcosParaAstilleroMilitar.Submarino;
+import Modelo.Tablero;
+
+public class Main {
+    public static void main(String[] args){
+        Submarino s1 = new Submarino();
+        Tablero t1 = new Tablero(13);
+        try {
+            t1.setBarco(s1,'N',6,6);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        t1.activarTablero();
+        try {
+            t1.dispararUna(6,6);
+        } catch (InvalidDisparoException e) {
+            e.printStackTrace();
+        }
+    }
+}
